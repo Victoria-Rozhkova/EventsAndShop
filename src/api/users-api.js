f75f144d899fed737b9b7d08b231a95e8449bb8f
Victoria Rozhkova <victoria.rozhkova@ro.ru>
2022-09-14 20:51:44 +0400

    preview

--- src/api/users-api.js
+++
@@ -1,9 +0,0 @@
-export const UsersAPI = {
-  async getUsers(limit = 5, offset = 0) {
-    const res = await fetch(
-      `https://test.relabs.ru/api/users/list?limit=${limit}&offset=${offset}`
-    );
-    const data = await res.json();
-    return data;
-  },
-};
